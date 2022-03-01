be15aab30e1b6e5a3458e4ecd2a52a9baacd7768
nf-timsu <tim.su@nextfortune.tw>
2022-03-01 18:46:26 +0800

    add discuss

--- tpot_digits_pipeline.py
+++ tpot_digits_pipeline.py
@@ -14,13 +14,13 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=77)
 
-# Average CV score on the training set was: 0.995771943884371
+# Average CV score on the training set was: 0.24942230160546322
 exported_pipeline = make_pipeline(
     make_union(
         FunctionTransformer(copy),
         FunctionTransformer(copy)
     ),
-    XGBClassifier(learning_rate=1.0, max_depth=7, min_child_weight=12, n_estimators=100, n_jobs=-1, subsample=0.3, verbosity=0)
+    XGBClassifier(learning_rate=1.0, max_depth=4, min_child_weight=9, n_estimators=100, n_jobs=-1, subsample=0.25, verbosity=0)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 77)
